0c4f72027ff5886a39f8f39a75489c912903f82c
Hemo9644 <144930248+Hemo9644@users.noreply.github.com>
2025-11-16 01:24:58 -0500

    Create fix_author.py

---
+++ fix_author.py
@@ -0,0 +1,8 @@
+# fix_author.py
+def email_callback(email):
+    if email == b"lingeswar909@gmail.com":
+        return b"saihemanth9644@gmail.com"
+    return email
+
+def name_callback(name):
+    return b"Hemo9644"
